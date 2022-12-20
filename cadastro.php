a6bf02517aa1c5f965644b6f6210cfbdef8cd6c0
Micael Cunha <micaaelvitoor2015@gmail.com>
2022-12-19 22:09:22 -0300

    senai

---
+++ cadastro.php
@@ -0,0 +1,64 @@
+<?php 
+
+    include "_scripts/config.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style_cadastro.css">
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+
+    <title>Cadastro</title>
+
+</head>
+<body>
+    <div class="fut">
+        <div class="container">
+            <div class="label_login row1">
+                <h1> Cadastro </h1>
+            </div>
+            <form class="user" method="POST" action="_scripts/autenticar.php">
+                <div class="input_nome row1">
+                    <label for="">Nome</label><br>
+                    <input type="text" id="nome" placeholder="Digite seu Nome" name='nome' required>
+                </div>
+                <div class="input_cpf row1">
+                <label for="">CPF</label><br>
+                    <input type="text" id="cpf"
+                    placeholder="Digite seu CPF" name='cps' required>
+                </div>
+                <div class="input_email row1">
+                <label for="">E-mail</label><br>
+                    <input type="email" id="email" aria-describedby="emailHelp"
+                    placeholder="Digite seu e-mail" name='email' required>
+                </div>
+                <div class="input_telefone row1">
+                <label for="">Telefone</label><br>
+                    <input type="text" id="telefone"
+                    placeholder="Digite seu telefone" name='telefone' required>
+                </div>
+                <div class="input_senha row1">
+                <label for="">Senha</label><br>
+                    <input type="passoword" id="senha"
+                    placeholder="Digite sua senha" name='semha' required><br>
+                    <input type="passoword" id="confirma_senha" 
+                    placeholder="Comfirme a senha" name='confirma_senha' required>
+                </div>
+                <div class="btn_cadastrar row1">
+                    <button class="btn btn-primary btn-user btn-block">
+                        CADASTRAR
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- tamplates/fazer_aposta.php
+++ tamplates/fazer_aposta.php
@@ -1,8 +1,5 @@
 <div class="container">
             <div class="topo_main">
-                <a class="bnt_voltar sublinhado">
-                    <h3>Voltar</h3>
-                </a>
                 <div class="label_palpite">
                     <h3>Digite seu Palpite!!</h3>
                 </div>
@@ -11,12 +8,13 @@
             <?php 
             $sql = "SELECT * FROM dados_jogos";
             $query = $mysqli->query($sql) or die ($mysqli->error);
-            $i = 1;
-            $j = 2;
+            $a = 1;
             while ($dados = $query->fetch_array()){
             ?>
+            <form  method="POST" action="<?php include "_scripts/cad_aposta.php"; ?>">
                 <div class="partida">
-                    <div class="row linha1">
+                <input type="hidden" name="jogo" value="<?php echo $dados['id']; ?>">
+                    <div class="local row linha1">
                         <div class="estadio col format1">
                             <?php echo $dados['local']; ?>
                         </div>
@@ -27,29 +25,30 @@
                             <?php echo $dados['data']; ?>
                         </div>
                         <div class="rodada col format1">
-                            <?php echo $dados['rodada']; ?>
+                            <?php echo date("d/m/Y", strtotime($dados['rodada'])); ?>
                         </div>
                     </div>
 
                     <div class="container linha2">
-                        <div class="format2">
+                        <div class="timea format2">
                             <img src="images/bandeiras/<?php echo $dados['timea']; ?>.png" class="flag">
                             <?php echo $dados['timea']; ?>
                         </div>
 
-                        <div class="format2">
-                            <input type="text" id="fname" name="fname">
+                        <div class="input1 format2">
+                            <input type="text" id="t1"
+                            placeholder="0" name="t1" required required maxlength="2">
                         </div>
 
-                        <div class="">
+                        <div class="x_amarelo">
                             <img src="images/Xamarelo.png" class="xzinho">
                         </div>
 
-                        <div class="format2">
-                            <input type="text" id="fname" name="fname">
+                        <div class="input2 format2">
+                            <input type="text" id="t2" placeholder="0" name="t2" required maxlength="2">
                         </div>
 
-                        <div class="format2">
+                        <div class="timeb format2">
                             <?php echo $dados['timeb']; ?>
                             <img src="images/bandeiras/<?php echo $dados['timeb']; ?>.png" class="flag">
                         </div>
@@ -62,8 +61,13 @@
                         <div class="status col format3">
                             Status: <?php echo $dados['status_jogo']; ?>
                         </div>
+                        <div class="btn_confirmar col format3">
+                            <button type="">CONFIRMAR</button>
+                        </div>
                     </div>
 
                 </div>
-            <?php $i++;$j++;} ?>
+            </form>
+            
+            <?php $a++; } ?>
             </div>
